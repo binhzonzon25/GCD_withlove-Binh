93acacfce9f34db676551173abef5cf23916ed51
binhzonzon25 <pbinh2501@gmail.com>
2021-10-15 12:09:38 +0700

    Add project files.

---
+++ bai2/bai2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace bai2
+{
+    class bai2
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age:  ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your heigh(cm):  ");
+            int heigh = int.Parse(Console.ReadLine());
+            double h = Convert.ToDouble(heigh);
+
+
+            Console.Write("Enter your weight(kg):  ");
+            int weight = int.Parse(Console.ReadLine());
+            double w = Convert.ToDouble(weight);
+
+            double BMI = w /((h / 100) * (h / 100));
+
+            Console.Write("Enter your email:  ");
+            string email = Console.ReadLine();
+
+            Console.Write("In relationship (true/false): ");
+
+            bool check;
+            if (Console.ReadLine() == "true")
+                check = true;
+            else
+                check = false;
+
+            Console.WriteLine("----------------out put ---------------");
+
+            Console.WriteLine($"Name: {name} ");
+            Console.WriteLine($"Age: {age} ");
+            Console.WriteLine($"heght: {heigh} weight {weight}   BMI {Math.Round(BMI,2)} ");
+            Console.Write($"In relationship {check}  ");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*NHAP VAO TU CHUONG TRINH *");
+            Console.Write("Campus:");
+                string campus = Console.ReadLine();
+
+            Console.Write("Address:");
+                string add = Console.ReadLine();
+
+            Console.WriteLine("-----------------------");
+            Console.Write("Nhap vao cac thong tin sau: ");
+            Console.Write("Fullname: ");
+                string fullname = Console.ReadLine();
+
+            Console.Write("Birthday: ");
+                string birthday = Console.ReadLine();
+
+            Console.Write("Age: ");
+                string age = Console.ReadLine();
+
+            Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+            Console.Write("Boyfriend/Girl: ");
+                string bg = Console.ReadLine();
+
+            Console.WriteLine("--------------------------");
+    
+            Console.WriteLine("*IN RA TU CHUONG TRINH *");
+            Console.WriteLine("University of Greenwich               Campus: ",campus);
+            Console.WriteLine("Address: ",add);
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("*Nhap vao cac thong tin sau*");
+            Console.WriteLine("Fullname: ",fullname);
+            Console.WriteLine("Birthday: ", birthday);
+            Console.WriteLine("Age: ",age);
+            Console.WriteLine("Email: ",email);
+            Console.WriteLine("Boyfriend/Girl: ",bg);
+            Console.WriteLine("--------------------------");
+            Console.ReadLine();
+
+        }
+    }
+}
