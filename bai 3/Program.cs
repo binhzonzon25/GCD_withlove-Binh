6548a38c8cc670dd9e8792715af4d05a66af47ff
binhzonzon25 <pbinh2501@gmail.com>
2021-10-18 16:52:41 +0700

    bai TVN 3

---
+++ bai 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace bai_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("---------------------------");
+            Console.Write("Nhap n: ");
+            int num = int.Parse(Console.ReadLine());
+            for(int i =1; i< num; i++)
+            {
+                sum = sum + num;
+            }
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine("---------------------------");
+        for (int i=1; i<=num; i++)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine("");
+
+            }
+            Console.WriteLine("---------------------------");
+        for (int i=1; i<=10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");           
+            }    
+        }
+    }
+}
---
+++ test for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace test_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            /*int sum = 0;
+            for(int i=0; i<=num; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine($"{sum}");*/
+            for (int i=1; i<=10; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num*i}");
+            }    
+        }
+    }
+}
